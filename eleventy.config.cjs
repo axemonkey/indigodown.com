3ec7df0832e5c252325facfd2464331b9c6d15d6
axemonkey <hi+github@clivemurray.com>
2023-06-30 14:27:38 +0100

    COMPLETE RETOOL to 11ty

---
+++ eleventy.config.cjs
@@ -0,0 +1,29 @@
+const icons = require('svg-social-icons');
+
+const socialIconsSelection = ['email', 'twitter', 'instagram', 'facebook', 'itunes', 'spotify', 'youtube'];
+const socialIcons = {};
+
+for (const thisIcon of socialIconsSelection) {
+	socialIcons[thisIcon] = icons(thisIcon, {color: '#ffffff'});
+}
+
+module.exports = function (eleventyConfig) {
+	eleventyConfig.addPassthroughCopy('indigodown.com/public');
+	eleventyConfig.setUseGitIgnore(false);
+	eleventyConfig.setServerOptions({
+		// liveReload: false,
+		watch: [
+			'indigodown.com/public/**/*',
+		],
+		showVersion: true,
+	});
+
+	eleventyConfig.addNunjucksGlobal('socialIcons', socialIcons);
+
+	return {
+		dir: {
+			includes: "_includes",
+			layouts: "_layouts",
+		}
+	}
+};
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-import {indigodownApp} from './indigodown.com/app.js';
+// import {indigodownApp} from './indigodown.com/app.js';
 
-const env = process.env.NODE_ENV || 'development';
+// const env = process.env.NODE_ENV || 'development';
 
-if (env === 'development') {
-	indigodownApp.listen(7777);
-} else if (env === 'production') {
-	indigodownApp.listen(process.env.PORT);
-}
+// if (env === 'development') {
+// 	indigodownApp.listen(7777);
+// } else if (env === 'production') {
+// 	indigodownApp.listen(process.env.PORT);
+// }
