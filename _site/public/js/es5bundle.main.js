2a22eafdcf554139ddf4e477b05d5838813e2225
axemonkey <hi+github@clivemurray.com>
2025-08-06 12:37:38 +0100

    remove LAP banner

--- _site/public/js/es5bundle.main.js
+++ _site/public/js/es5bundle.main.js
@@ -1,11 +1,8 @@
 (function () {
 	'use strict';
 
-	// import {lapBanner} from './modules/lap-banner.js';
-
 	const init = () => {
 	  document.body.classList.add('js');
-	  // lapBanner.init();
 	};
 	window.addEventListener('load', init);
 
--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,8 +1,5 @@
-// import {lapBanner} from './modules/lap-banner.js';
-
 const init = () => {
 	document.body.classList.add('js');
-	// lapBanner.init();
 };
 
 window.addEventListener('load', init);
